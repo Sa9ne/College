82e2bb805f9c5a1bba0e7aa76c77b703ba70d8fc
Sa9ne <trealev@gmail.com>
2025-04-05 00:29:31 +0500

    Task3

--- Prack2/Para/Task3/3.go
+++ Prack2/Para/Task3/3.go
@@ -8,20 +8,20 @@ import (
 func main() {
 	var A, B, C float64
 
-	fmt.Print("Введите коэффициент A: ")
+	fmt.Print("Введите коэффициент при x^2: ")
 	fmt.Scan(&A)
-	fmt.Print("Введите коэффициент B: ")
+	fmt.Print("Введите коэффициент при x: ")
 	fmt.Scan(&B)
-	fmt.Print("Введите свободный C: ")
+	fmt.Print("Введите свободный коэффициент: ")
 	fmt.Scan(&C)
 
 	Disck := B*B - 4*A*C
 	fmt.Printf("Дискриминант равен: %.2f", Disck)
 
 	if Disck > 0 {
-		x1 := (-B + math.Sqrt(Disck)/(2*A))
-		x2 := (-B - math.Sqrt(Disck)/(2*A))
-		fmt.Printf("\nДва корня:%2.F и%2.F", x1, x2)
+		x1 := ((-B + math.Sqrt(Disck)) / (2 * A))
+		x2 := ((-B - math.Sqrt(Disck)) / (2 * A))
+		fmt.Printf("\nДва корня:%2.F и %2.F", x1, x2)
 	} else if Disck == 0 {
 		x := -B / (2 * A)
 		fmt.Printf("\nКорень равен: %2.F", x)
