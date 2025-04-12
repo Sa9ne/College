8a9bbf33c6a77fd859d10bb335e3316002964b7f
Sa9ne <trealev@gmail.com>
2025-04-12 13:23:17 +0500

    6 task

---
+++ Prack3/Task6/6.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// Граница генерации чисел
+	var min, max int
+
+	fmt.Print("Введите нижнюю границу генерации: ")
+	fmt.Scan(&min)
+	fmt.Print("Введите верхнюю границу генерации: ")
+	fmt.Scan(&max)
+	// Генерация числа
+	Random := rand.Intn(max-min+1) + min
+	// Проверка угадал ли пользователь
+	NumCheck := false
+	NumFound := 0
+
+	for !NumCheck {
+		fmt.Print("Угадайте число: ")
+		fmt.Scan(&NumFound)
+
+		if NumFound > max || NumFound < min {
+			fmt.Println("Вы ввели значение больше/меньше границы")
+			NumCheck = false
+			continue
+		}
+
+		if NumFound == Random {
+			NumCheck = true
+			break
+		} else {
+			fmt.Println("Не угадал")
+			NumCheck = false
+		}
+	}
+
+	// Вывод
+	fmt.Println("Молодец, число и вправду: ", NumFound)
+}
