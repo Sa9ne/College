4c0382a3e44e0b002f6b1fd96cdf0ca71c24ccd9
Sa9ne <trealev@gmail.com>
2025-04-19 22:13:02 +0500

    Task 1 complied

--- Prack4/Task1/1.go
+++ Prack4/Task1/1.go
@@ -2,8 +2,34 @@ package main
 
 import "fmt"
 
-func DeletedNum(DeleteNum int) {
+func DeletedNum(Num int, DeletedNum int) {
+	GoodNum := 0
 
+	// Алгоритм для уничтожения ненужного числа
+	for Num > 0 {
+		if Num%10 != DeletedNum {
+			// Проверка на первый элемент
+			if GoodNum > 0 {
+				GoodNum *= 10
+				GoodNum += Num % 10
+			} else {
+				GoodNum = Num % 10
+			}
+			Num /= 10
+		} else {
+			Num /= 10
+		}
+	}
+
+	// Так как у нас получился перевернутый элемент, переворачиваем его
+	result := 0
+
+	for GoodNum != 0 {
+		result = result*10 + GoodNum%10
+		GoodNum /= 10
+	}
+
+	fmt.Println(result)
 }
 
 func main() {
@@ -15,7 +41,12 @@ func main() {
 	fmt.Print("Введите цифру для удаления: ")
 	fmt.Scan(&DeleteNum)
 
+	if DeleteNum/10 != 0 {
+		fmt.Println("Что то много чисел для удаления")
+		return
+	}
+
 	// Вызов функции и результат
-	DeletedNum(DeleteNum)
+	DeletedNum(Num, DeleteNum)
 
 }
